ae634fba149f6fc39ead9020e7b1afe8670539ef
Jscosta7 <joaoescosta.20231@poli.ufrj.br>
2024-06-04 23:36:05 -0300

    inicio pagina login\cadastro

--- pame24.1-entrega-3/src/app/page.tsx
+++ pame24.1-entrega-3/src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (<div></div>);
+  return (<div className="bg-black w-screen h-[100vh] flex items-center justify-center">
+    <div className="w-[50vw] h-[50vh] bg-zinc-950 shadow-md rounded-lg flex items-center justify-center">
+      <input className="w-[30vw] h-[5vh] text-gray-400"> Nome de usuário ou Email</input>
+    </div>
+  </div>
+);
 }
